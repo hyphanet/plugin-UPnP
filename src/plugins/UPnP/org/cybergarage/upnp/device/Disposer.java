80dcf1c2d468e0065860f820ce681d600c488647
Florent Daignière <nextgens@freenetproject.org>
2007-03-26 00:53:45 +0000

    plugins: UPnP: implement basic stuffs allowing to see the feasability of such a plugin...
    I've already spent my WE on it, and it doesn't work for me! :(
    
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/UPnP@12375 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/plugins/UPnP/org/cybergarage/upnp/device/Disposer.java
@@ -0,0 +1,65 @@
+/******************************************************************
+*
+*	CyberLink for Java
+*
+*	Copyright (C) Satoshi Konno 2002-2004
+*
+*	File: Disposer.java
+*
+*	Revision:
+*
+*	01/05/04
+*		- first revision.
+*	
+******************************************************************/
+
+package plugins.UPnP.org.cybergarage.upnp.device;
+
+import plugins.UPnP.org.cybergarage.upnp.*;
+import plugins.UPnP.org.cybergarage.util.*;
+
+public class Disposer extends ThreadCore
+{
+	////////////////////////////////////////////////
+	//	Constructor
+	////////////////////////////////////////////////
+
+	public Disposer(ControlPoint ctrlp)
+	{
+		setControlPoint(ctrlp);
+	}
+	
+	////////////////////////////////////////////////
+	//	Member
+	////////////////////////////////////////////////
+
+	private ControlPoint ctrlPoint;
+
+	public void setControlPoint(ControlPoint ctrlp)
+	{
+		ctrlPoint = ctrlp;
+	}
+	
+	public ControlPoint getControlPoint()
+	{
+		return ctrlPoint;
+	}
+
+	////////////////////////////////////////////////
+	//	Thread
+	////////////////////////////////////////////////
+	
+	public void run() 
+	{
+		ControlPoint ctrlp = getControlPoint();
+		long monitorInterval = ctrlp.getExpiredDeviceMonitoringInterval() * 1000;
+		
+		while (isRunnable() == true) {
+			try {
+				Thread.sleep(monitorInterval);
+			} catch (InterruptedException e) {}
+			ctrlp.removeExpiredDevices();
+			//ctrlp.print();
+		}
+	}
+}
